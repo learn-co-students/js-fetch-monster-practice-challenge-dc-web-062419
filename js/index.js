b63dd63ffd08a3d108ba32f8edb82e5947c0de82
Harum Helmy <harumendhah@gmail.com>
2019-08-09 16:49:04 -0400

    got form to work and persist to database

--- js/index.js
+++ js/index.js
@@ -9,10 +9,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
 function getMonsters() {
     fetch("http://localhost:3000/monsters?_limit=50&_page=1")
     .then(resp => resp.json())
-    .then(monsters => monsters.forEach(renderMonster))
+    .then(monsters => monsters.forEach(addMonsterToDOM))
 }
 
-function renderMonster(monster){
+// function monsterUrl() {
+//     return `http://localhost:3000/monster?_limit=50&page=${pageNum}`
+// }
+
+function addMonsterToDOM(monster){
     const monsterDiv = document.createElement('div')
     const monsterH2 = document.createElement('h2')
     const monsterH4 = document.createElement('h4')
@@ -56,17 +60,31 @@ function createNewMonster(e) {
     const newName= e.target["name"].value
     const newAge = e.target["age"].value
     const newDesc = e.target["description"].value
-    if (newName === '' || newAge === '') {
-        alert('Please fill out both name and image')
+
+    if (newName === '' || newAge === '' || newDesc === '') {
+        alert('Please fill out all form inputs')
       } else {
-        const data = {
-          name: e.target['name'].value,
-          age: e.target['age'].value,
-          likes: 0
+        const monsterData = {
+          name: newName,
+          age: newAge,
+          description: newDesc
         }
+    postNewMonster(monsterData)
+    document.getElementById("monster-form").reset()
+    }
 }
 
-function postNewMonster() {
-
+function postNewMonster(monsterData) {
+    fetch("http://localhost:3000/monsters/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            'accept': "application/json"
+        },
+        body: JSON.stringify(monsterData)
+    })
+    .then(res => res.json())
+    .then(addMonsterToDOM)
+    .catch(()=>alert('Server down! Please add your monster later <3'))
 }
 
